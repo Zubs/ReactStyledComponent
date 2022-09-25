be0e38b2179c96d4c809e4a9db5c8f10e993cd21
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-25 15:01:21 +0100

    pseudo classes

--- src/styled-components/Button.js
+++ src/styled-components/Button.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledButton = styled.button`
   border: 2px solid #4caf50;
   background-color: ${ (props) => (props.variant === 'outline' ? 'white' : '#4caf50') };
-  color: ${ (props) => (props.variant === 'outline' ? '#4caf50' : 'white') };
+  color: ${ (props) => (props.variant !== 'outline' ? 'white' : '#4caf50') };
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
@@ -11,8 +11,13 @@ export const StyledButton = styled.button`
   font-size: 16px;
   cursor: pointer;
   transition: 0.5s all ease-out;
+	
+	&:hover {
+    background-color: ${ (props) => (props.variant !== 'outline' ? 'white' : '#4caf50') };
+    color: ${ (props) => (props.variant === 'outline' ? 'white' : '#4caf50') };
+	}
 `;
 
 export const FancyButton = styled(StyledButton)`
-	background-image: linear-gradient(to right, white, #f2f2f2);
+	background-image: linear-gradient(to right, white, black);
 `;
