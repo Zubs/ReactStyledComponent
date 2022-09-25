dd90fe1b2cba400659e3fb9aedbe4eb767246c8a
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-25 13:23:37 +0100

    Button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import StyledButton from "./styled-components/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <StyledButton>Testing Again</StyledButton>
     </div>
   );
 }
---
+++ src/styled-components/Button.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: 2px solid #4caf50;
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  transition: 0.5s all ease-out;
+`;
+
+export default StyledButton;
