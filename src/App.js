8afd21981f4fbc6774b78e3eddd8e3ba9905a3ad
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-25 14:44:09 +0100

    Dynamic styling with props

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <StyledButton>Testing Again</StyledButton>
+      <br/>
+      <StyledButton variant="outline">Testing Again</StyledButton>
     </div>
   );
 }
--- src/styled-components/Button.js
+++ src/styled-components/Button.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   border: 2px solid #4caf50;
-  background-color: #4caf50;
-  color: white;
+  background-color: ${ (props) => (props.variant === 'outline' ? 'white' : '#4caf50') };
+  color: ${ (props) => (props.variant === 'outline' ? '#4caf50' : 'white') };
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
