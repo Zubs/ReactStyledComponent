3d9338c4c08f1a96f69bd2ed8bd63b44c244c9e9
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-09-25 15:22:16 +0100

    Basic theming

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
-import { FancyButton, StyledButton, SubmitButton } from "./styled-components/Button";
+import { ThemeProvider } from 'styled-components';
+import {
+  DarkButton,
+  FancyButton,
+  StyledButton,
+  SubmitButton
+} from "./styled-components/Button";
+
+const theme = {
+  dark: {
+    primary: 'black',
+    text: 'white'
+  },
+  light: {
+    primary: 'white',
+    text: 'black'
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <StyledButton>Testing Again</StyledButton>
-      <br/>
-      <StyledButton variant="outline">Testing Again</StyledButton>
-      <br/>
-      <FancyButton as='a'>Fancy Testing</FancyButton>
-      <br/>
-      <SubmitButton>Submit me</SubmitButton>
-    </div>
+    <ThemeProvider theme={ theme }>
+      <div className="App">
+        <h1>Hello World</h1>
+        <StyledButton>Testing Again</StyledButton>
+        <br/>
+        <StyledButton variant="outline">Testing Again</StyledButton>
+        <br/>
+        <FancyButton as='a'>Fancy Testing</FancyButton>
+        <br/>
+        <SubmitButton>Submit me</SubmitButton>
+        <br/>
+        <br/>
+        <DarkButton>Lmao</DarkButton>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/styled-components/Button.js
+++ src/styled-components/Button.js
@@ -33,3 +33,14 @@ export const SubmitButton = styled(StyledButton).attrs({
 		transform: translateY(4px);
 	}
 `;
+
+export const DarkButton = styled(StyledButton)`
+  border: 2px solid ${ props => props.theme.dark.primary };
+  background-color: ${ props => props.theme.dark.primary };
+  color: ${ props => props.theme.dark.text };
+	
+	&:hover {
+    background-color: ${ props => props.theme.dark.text };
+    color: ${ props => props.theme.dark.primary };
+	}
+`;
